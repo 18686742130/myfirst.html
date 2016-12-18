8ddd5e53519291f6ff2ff3d3ecd4aa0824cbda66
lenovo <615123406@qq.com>
2016-12-18 18:16:13 +0800

    练习

--- nodejs/myblog/routes/index.js
+++ nodejs/myblog/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var user = require('../controllers/user');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { uname: null });
+router.get('/',function(req,res){
+    res.render('index')
 });
+router.get('/login', user.login);
+router.get('/reg', user.reg);
+router.post('/checkLogin', user.checkLogin);
+
+
 
 module.exports = router;
